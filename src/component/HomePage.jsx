0f66348d9d26b74f77a1d48edb32a7fe026b0806
sukanya <sukanyajakate4@gmail.com>
2023-03-07 14:46:56 +0530

    minor

--- src/component/HomePage.jsx
+++ src/component/HomePage.jsx
@@ -25,7 +25,7 @@ const HomePage = () => {
             </div>
 
             <div>
-                <img src={HeroImage} alt="My Profile" className="  rounded-2xl mx-auto w-2/3 md:full"/>
+                <img src={HeroImage} alt="My Profile" className="  rounded-2xl mx-auto w-full bg-transperant md:full"/>
             </div>
         </div>
     </div>
